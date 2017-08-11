daf4cabb6030268b2b1083e9828b30c65a38d7d2
Anish <Anish@Anish-PC>
2017-08-11 17:36:13 +0530

    Adding module conf's.

---
+++ adapter/src/main/java/org/framework/adapter/contracts/config/ConfigConst.java
@@ -0,0 +1,13 @@
+package org.framework.adapter.contracts.config;
+
+/**
+ * This interface contains the constant for Adapter Frameworks.
+ * 
+ * @author Anish Singh
+ *
+ */
+public interface ConfigConst {
+
+	final String MODULE_CONFIG_FILE_SUFFIX = "-module-conf.xml";
+
+}
